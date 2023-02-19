67258021dbed481f6926b47570e2892576a61b73
pavels64 <kudrjavcevspavels@gmail.com>
2023-02-19 19:52:42 +0200

    ()

--- main.py
+++ main.py
@@ -11,20 +11,25 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+    for i, next_char in enumerate(text):
+        if next_char in "([{":
+            opening_brackets_stack.append(Bracket(next_char, i+1))
+        elif next_char in ")]}":
+            if not opening_brackets_stack:
+                return i+1
+            last_opening_bracket = opening_brackets_stack.pop()
+            if not are_matching(last_opening_bracket.char, next_char):
+                return i+1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    else:
+        return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
